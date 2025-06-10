3459593f9b96b709fc2231a201f6f7685b846692
Anarkopete <juaninwii@gmail.com>
2025-06-10 19:58:25 -0400

    colormaker
    colormaker

--- src/Calculator/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Calculator.AssemblyInfo.cs
+++ src/Calculator/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+293edc3787194607b8b4d413947787bc95dde782")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
